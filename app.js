b7b32a13da5e335ca188882d9d02229a8386c442
Michael Collier <colliermichaelp@gmail.com>
2015-05-18 08:33:21 -0600

    Going to Hawaii
    Committing so I can have my work online INCASE I want to work on it.

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 var shipList = [];
 var shipTotal = 0;
+var maxPrice = 0;
+var minPrice = 0;
 
 $(document).ready(function(){
 	$(".previousOwnerButton").hide();
 
 	var apiCall = "http://swapi.co/api/starships/";
 	hitAPI(apiCall);
+
+	//Bind functions
+	$("#searchText").on('keyup', function(evt){
+		disableControls(0);
+		searchFilter(this);
+	});
+
+	$("#filterPrice").on('keyup', function(evt){
+		disableControls(1);
+		priceFilter(this);
+	});
+
+	$("#showUnknownPriceCheckbox").on('change', function(evt){
+		disableControls(2);
+		showUnknownPrice(this.checked);
+	});
 });
 
 
@@ -19,9 +37,10 @@ var onResponse = function(dataObject){
 	//Update ShipList
 	for (i = 0; i<dataObject.results.length; i++){
 		shipList[shipTotal] = dataObject.results[i];
-		//console.log("Ship : " + dataObject.results[i].name + " added at index " + shipTotal);
 		$(".shipListGroup").append("<div class=\"list-group-item\" onclick=\"inspectShip(this," + shipTotal + ")\">" + shipList[shipTotal].name + "</div>");
+		//console.log("Ship : " + dataObject.results[i].name + " added at index " + shipTotal);
 		shipTotal++;
+
 	}
 
 	//Get Next Pages
@@ -41,7 +60,7 @@ var inspectShip = function(e, shipNumber) {
 		//Disactivate previously activated element
 		$(".list-group .list-group-item").removeClass("active");
 
-		//Remove any already highlighted ship
+		//Remove old ship data
 		$(".shipDetailsList").empty();
 		$(".previousOwnerButton").hide();
 
@@ -77,17 +96,12 @@ var inspectShip = function(e, shipNumber) {
 
 		//THE PRESTIGE
 		$(".shipDetailsList").show("fast");
-		//console.log(shipList[shipNumber].pilots.length);
-		//console.log(shipList[shipNumber]);
 		if(shipList[shipNumber].pilots.length > 0)
 		{
 			$(".previousOwnerButton").show("fast");
 			$(".previousOwnerButton").on('click', function() {
-				//console.log(shipNumber);
-				//console.log(shipList[shipNumber].pilots.length);
-				$(".pilotsRow").empty();
+				$(".modal-body").empty();
 				for (i = 0; i < shipList[shipNumber].pilots.length; i++) {
-					//console.log(i + "/" + shipList[shipNumber].pilots.length);
 					$.get(shipList[shipNumber].pilots[i], gotCrap);
 				}
 			});
@@ -97,7 +111,70 @@ var inspectShip = function(e, shipNumber) {
 }
 
 var gotCrap = function(pilotObject) {
-	//console.log("Getting Previous Owner info for ship number: " + shipIndex);
-	$(".pilotsRow").append("<div class=\"col-sm-2\" >" + pilotObject.name + "</div>");
+	var homeworldObject = $.ajax(pilotObject.homeworld, {async: false});
+	$(".modal-body").append("<div class=\"well-sm\"> " + pilotObject.name + "</div>");
+	$(".modal-body").append("Homeworld: " + homeworldObject.responseJSON.name);
+	$(".modal-body").append("<hr>");
+}
+
+var searchFilter = function(e) {
+	//Dat Big O notation... Let's just ignore that for this function.
+	//console.log(document.getElementById("shipListGroup").childNodes);
+
+	for (i = 0; i < shipList.length; i++){
+		if (!shipList[i].name.toLowerCase().includes(e.value.toLowerCase())) {
+			$(document.getElementById("shipListGroup").childNodes[i+1]).hide("fast");
+		} else {
+			$(document.getElementById("shipListGroup").childNodes[i+1]).show("fast");
+		}
+	}
+}
+
+var priceFilter = function(e) {
+	for (i = 0; i < shipList.length; i++){
+			if (Number(e.value) > Number(shipList[i].cost_in_credits)) {
+				$(document.getElementById("shipListGroup").childNodes[i+1]).show("fast");
+			} else {
+				$(document.getElementById("shipListGroup").childNodes[i+1]).hide("fast");
+			}
+		}
+}
+
+var showUnknownPrice = function(hide){
+	if (!hide) {
+		for (i=0; i < shipList.length; i++) {
+			if (shipList[i].cost_in_credits == "unknown") {
+				$(document.getElementById("shipListGroup").childNodes[i+1]).show("fast");
+			}
+		}
+	} else {
+		for (i=0; i < shipList.length; i++) {
+			if (shipList[i].cost_in_credits == "unknown") {
+				$(document.getElementById("shipListGroup").childNodes[i+1]).hide("fast");
+			}
+		}
+	}
+}
+
+var disableControls = function(controlCode) {
+	switch(controlCode){
+		//Except search
+		case 0:
+			$("#showUnknownPriceCheckbox").addClass("disabled");
+			$("#filterPrice").addClass("disabled");
+			break;
+		//Except priceFilter
+		case 1:
+			$("#searchText").addClass("disabled");
+			$("#showUnknownPriceCheckbox").addClass("disabled");
+			break;
+		//Except UnknownPrice
+		case 2:
+			$("#searchText").addClass("disabled");
+			$("#filterPrice").addClass("disabled");
+			break;
+		deafult:
+				break;
+	}
 
 }
